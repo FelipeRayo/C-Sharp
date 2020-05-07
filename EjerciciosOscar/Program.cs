fd1a3b4bc2406f60c347ed2907938337147f3926
Felipe Rayo <piperayo062001@gmail.com>
2020-05-07 12:30:06 -0500

    Primeros ejercicios

---
+++ EjerciciosOscar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EjerciciosOscar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greeting("Hello World"));
+            Console.WriteLine(UnoporMi("Felipe"));
+            Console.ReadKey();
+        }
+
+        static string Greeting(string greeting)
+        {
+            string saludo = greeting;
+
+            return saludo;
+        }
+
+
+        static string UnoporMi(string nombre)
+        {
+            string respuesta;
+            if (nombre == String.Empty)
+            {
+                 respuesta = "Uno por ti, uno por mi";
+            }
+            else
+            {
+                 respuesta = "Uno por " + nombre + ", uno por mi";
+            }
+
+            return respuesta;
+
+        }
+    }
+}
